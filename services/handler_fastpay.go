979ba532a0ac186a7d9c305dcf533fd8c7ab50fc
nuriaputri <putrinuria67@gmail.com>
2020-11-13 10:37:44 +0700

    first commit

---
+++ services/handler_fastpay.go
@@ -0,0 +1,14 @@
+package services
+
+import (
+	"context"
+
+	cm "FrameworkNuria/common"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func (PaymentService) FastPayHandler(ctx context.Context, req cm.FastPayRequest) (res cm.FastPayResponse) {
+
+	return
+}
